6209b284404151bbdcfd6466e8b95b8a86cebf39
Matt Schwennesen <mjschwenne@gmail.com>
2024-09-11 14:45:05 -0500

    rework timestamp proofs to stateless style

--- example/marshal_proof.v
+++ example/marshal_proof.v
@@ -24,19 +24,20 @@ Definition own args_ptr args q : iProp Σ :=
   "Hargs_minute" ∷ args_ptr ↦[TimeStamp :: "minute"]{q} #args.(minute) ∗
   "Hargs_second" ∷ args_ptr ↦[TimeStamp :: "second"]{q} #args.(second).
 
-Lemma wp_Encode (args_ptr:loc) (args:Timestamp) :
+Lemma wp_Encode (args_ptr:loc) (args:Timestamp) (prefix:list u8) (pre_sl:Slice.t) :
   {{{
-        own args_ptr args (DfracDiscarded)
+        "Hown" ∷ own args_ptr args (DfracDiscarded) ∗
+        "Hpre" ∷ own_slice pre_sl byteT (DfracOwn 1) prefix
   }}}
-    MarshalTimeStamp #args_ptr
+    MarshalTimeStamp #args_ptr (slice_val pre_sl)
   {{{
         enc enc_sl, RET (slice_val enc_sl);
         ⌜has_encoding enc args⌝ ∗
-        own_slice enc_sl byteT (DfracOwn 1) enc
+        own_slice enc_sl byteT (DfracOwn 1) (prefix ++ enc)
   }}}.
 
 Proof.
-  iIntros (?) "H HΦ". iNamed "H". wp_rec.
+  iIntros (?) "H HΦ". iNamed "H". iNamed "Hown". wp_rec.
   wp_apply (wp_NewSliceWithCap).
   { apply encoding.unsigned_64_nonneg. }
   iIntros (?) "Hsl".
@@ -53,17 +54,20 @@ Proof.
   wp_loadField. wp_load. wp_apply (wp_WriteInt32 with "[$]").
   iIntros (?) "Hsl". wp_store.
 
-  wp_load. iApply "HΦ". iFrame. iPureIntro. done.
+  wp_load. wp_apply (wp_SliceAppendSlice with "[Hpre Hsl]"); first auto.
+  { iFrame. iApply own_slice_to_small in "Hsl". iFrame. }
+  iIntros (?) "[Hs1 Hs2]". iApply "HΦ". iFrame. iPureIntro. done.
 Qed.
 
-Lemma wp_Decode enc enc_sl (args:Timestamp) :
+Lemma wp_Decode enc enc_sl (args:Timestamp) (suffix : list u8) (q : dfrac):
   {{{
         ⌜has_encoding enc args⌝ ∗
-        own_slice_small enc_sl byteT (DfracOwn 1) enc
+        own_slice_small enc_sl byteT q (enc ++ suffix)
   }}}
     UnmarshalTimeStamp (slice_val enc_sl)
   {{{
-        args_ptr, RET #args_ptr; own args_ptr args (DfracOwn 1)
+        args_ptr suff_sl, RET (#args_ptr, suff_sl); own args_ptr args (DfracOwn 1) ∗
+                                                    own_slice_small suff_sl byteT q suffix
   }}}.
 
 Proof.
@@ -84,6 +88,7 @@ Proof.
   wp_load. wp_apply (wp_ReadInt32 with "[$]"). iIntros (?) "Hs".
   wp_pures. wp_storeField. wp_store.
 
+  wp_load. wp_pures.
   iApply "HΦ". iModIntro. iFrame.
 Qed.
 
