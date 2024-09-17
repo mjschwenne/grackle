d2347334fde228edbf7b287a611baa2239a639a2
Matt Schwennesen <mjschwenne@gmail.com>
2024-09-16 21:14:03 -0500

    Generate definitions for non-nested messages
    Note that templates with {{ for fields still need to be updated.

--- src/parse.go
+++ src/parse.go
@@ -2,21 +2,90 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
 
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/descriptorpb"
 )
 
-var typeMap = map[descriptorpb.FieldDescriptorProto_Type]string{
+type field = descriptorpb.FieldDescriptorProto
+type fieldType = descriptorpb.FieldDescriptorProto_Type
+
+type CoqOutput struct {
+	ProjectName string
+	Name        string
+	GooseOutput string
+	ModuleName  string
+	Fields      []*field
+}
+
+var typeMap = map[fieldType]string{
 	descriptorpb.FieldDescriptorProto_TYPE_INT32:   "u32",
 	descriptorpb.FieldDescriptorProto_TYPE_UINT32:  "u32",
 	descriptorpb.FieldDescriptorProto_TYPE_FIXED32: "u32",
 	descriptorpb.FieldDescriptorProto_TYPE_INT64:   "u64",
 	descriptorpb.FieldDescriptorProto_TYPE_UINT64:  "u64",
 	descriptorpb.FieldDescriptorProto_TYPE_FIXED64: "u64",
-	descriptorpb.FieldDescriptorProto_TYPE_MESSAGE: "{{ .GetTypeName }}",
+	descriptorpb.FieldDescriptorProto_TYPE_MESSAGE: "message",
+}
+
+func getCoqTypeName(field *field) string {
+	if field.GetType() == descriptorpb.FieldDescriptorProto_TYPE_MESSAGE {
+		return field.GetTypeName()
+	}
+	return typeMap[field.GetType()]
+}
+
+func getEncoding(field *field) string {
+	coqType, ok := typeMap[field.GetType()]
+	if !ok {
+		panic("Attempted to get encoding of unsupported type")
+	}
+
+	var result string
+	switch coqType {
+	case "u32":
+		result = fmt.Sprintf("(u32_le args.(%s))", field.GetName())
+	case "u64":
+		result = fmt.Sprintf("(u64_le args.(%s))", field.GetName())
+	case "message":
+		result = fmt.Sprintf("MESSAGES NOT SUPPORTED YET\n")
+	}
+
+	return result
+}
+
+func filter[T any](slice []T, f func(T) bool) []T {
+	var n []T
+	for _, e := range slice {
+		if f(e) {
+			n = append(n, e)
+		}
+	}
+	return n
+}
+
+func generateHasEncodingExistsQuant(fields []*field) string {
+	nestedMessages := filter(fields, func(f *field) bool {
+		return f.GetType() == descriptorpb.FieldDescriptorProto_TYPE_MESSAGE
+	})
+
+	if len(nestedMessages) == 0 {
+		return ""
+	}
+
+	resultBuilder := strings.Builder{}
+	resultBuilder.WriteString("∃ (")
+	for _, nestedMessage := range nestedMessages {
+		resultBuilder.WriteString(fmt.Sprintf("%s_l", nestedMessage.GetName()))
+	}
+	resultBuilder.WriteString(": loc),\n")
+	return resultBuilder.String()
 }
 
 func main() {
@@ -37,19 +106,40 @@ func main() {
 		log.Fatalf("Failed to unmarshal descriptor file: %v", err)
 	}
 
-	// Access the first FileDescriptorProto
-	if len(fileDescriptorSet.File) > 0 {
-		messages := fileDescriptorSet.File[0].MessageType
-		for _, m := range messages {
-			fields := m.GetField()
-			// Print some information about the protobuf
-			fmt.Printf("---------------------------------------\nMessage: %s\n---------------------------------------\n", m.GetName())
-			for i, f := range fields {
-				fmt.Printf("Field %v: %s of type %v (%s)\n", i, f.GetName(), f.GetType(), f.GetTypeName())
-			}
+	var tmplFiles []string
+	err = filepath.WalkDir("src/templates/", func(path string, info fs.DirEntry, err error) error {
+		if !info.IsDir() {
+			tmplFiles = append(tmplFiles, path)
 		}
+		return nil
+	})
 
-	} else {
-		log.Println("No file descriptors found in the set.")
+	funcMap := template.FuncMap{
+		"coqType":  getCoqTypeName,
+		"encoding": getEncoding,
+		"exists":   generateHasEncodingExistsQuant,
+	}
+	tmpl, err := template.New("coq_record").Funcs(funcMap).Delims("<<", ">>").ParseFiles(tmplFiles...)
+	if err != nil {
+		panic(err)
+	}
+
+	ts := fileDescriptorSet.File[0].MessageType[0]
+	var fields []*field
+	for _, f := range ts.GetField() {
+		fields = append(fields, f)
+	}
+
+	co := CoqOutput{
+		ProjectName: "Grackel.example",
+		Name:        *ts.Name,
+		GooseOutput: "example",
+		ModuleName:  *ts.Name,
+		Fields:      fields,
+	}
+
+	err = tmpl.ExecuteTemplate(os.Stdout, "coq.tmpl", co)
+	if err != nil {
+		panic(err)
 	}
 }
