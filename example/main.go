50308ebc5908d87eac5e48e9f9ced578466d5628
Matt Schwennesen <mjschwenne@gmail.com>
2025-01-28 14:33:02 -0600

    fix outdated example/main.go

--- example/main.go
+++ example/main.go
@@ -13,7 +13,7 @@ func main() {
 	event := Event{id: 1, name: "test event", startTime: TimeStamp{hour: 13, minute: 00, second: 00}, endTime: TimeStamp{hour: 14, minute: 00, second: 00}}
 	fmt.Printf("Name:  %s\nStart: %02d:%02d:%02d\nEnd:   %02d:%02d:%02d\n", event.name, event.startTime.hour, event.startTime.minute, event.startTime.second, event.endTime.hour, event.endTime.minute, event.endTime.second)
 
-	enc := MarshalEvent(event, []byte{})
+	enc := MarshalEvent([]byte{}, event)
 
 	var newEvent Event
 	newEvent, _ = UnmarshalEvent(enc)
