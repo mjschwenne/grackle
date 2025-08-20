38b9fcc7057cbb8043d1395c2bbb1a48ad3e772e
Matt Schwennesen <mjschwenne@gmail.com>
2025-08-20 17:20:59 -0500

    fix grackle nix build

--- nix/grackle/default.nix
+++ nix/grackle/default.nix
@@ -6,7 +6,7 @@
 buildGoModule {
   pname = "grackle";
   version = "new";
-  src = ../.;
+  src = ../../.;
   vendorHash = "sha256-tx+UiQWfXq9AJ/JQdGy9qw7VTKKVQ8D1CFAyN/wNU8w=";
   nativeBuildInputs = [protobuf];
 }
