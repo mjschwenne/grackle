2acd0901026c7656c0a39a00ce5732ec57df5b2e
Matt Schwennesen <mjschwenne@gmail.com>
2025-09-18 17:00:58 -0500

    rename testdata/out/coq -> testdata/out/rocq

--- run_tests.fish
+++ run_tests.fish
@@ -3,12 +3,12 @@
 echo "Cleaning old tests..."
 rm -rf testdata/out/*
 echo "Running grackle..."
-go run ./cmd/grackle/ -coq-logical-path Grackle.test -coq-physical-path testdata/out/coq/ -proofgen-path testdata/out/pg -go-output-path testdata/out/go -go-package github.com/mjschwenne/grackle/testdata/out/go -goose-output testdata/out/goose/ testdata/proto/calendar
-go run ./cmd/grackle/ -coq-logical-path Grackle.test -coq-physical-path testdata/out/coq/ -proofgen-path testdata/out/pg -go-output-path testdata/out/go -go-package github.com/mjschwenne/grackle/testdata/out/go -goose-output testdata/out/goose/ testdata/proto/complete
+go run ./cmd/grackle/ -rocq-logical-path Grackle.test -rocq-physical-path testdata/out/rocq/ -proofgen-path testdata/out/pg -go-output-path testdata/out/go -go-package github.com/mjschwenne/grackle/testdata/out/go -goose-output testdata/out/goose/ testdata/proto/calendar
+go run ./cmd/grackle/ -rocq-logical-path Grackle.test -rocq-physical-path testdata/out/rocq/ -proofgen-path testdata/out/pg -go-output-path testdata/out/go -go-package github.com/mjschwenne/grackle/testdata/out/go -goose-output testdata/out/goose/ testdata/proto/complete
 echo "Checking grackle go..."
 go build (find ./testdata/out/go/ -mindepth 1 -maxdepth 1 -type d)
-echo "Checking grackle coq..."
+echo "Checking grackle rocq..."
 rm .rocqdeps.d
-make -j(nproc) -s (find testdata/out/coq -name "*.v" | sed -e "s/\.v\$/\.vo/g")
+make -j(nproc) -s (find testdata/out/rocq -name "*.v" | sed -e "s/\.v\$/\.vo/g")
 echo "Running grackle tests..."
 go test .
